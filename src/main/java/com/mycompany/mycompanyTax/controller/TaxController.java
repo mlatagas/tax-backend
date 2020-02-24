64076528ed9364dec5f85c5c49c6c0ee7422fb8e
mthobisi <mthobisid401@gmail.com>
2020-02-24 16:11:18 +0200

    removed unnecessary param enabled CORS

--- src/main/java/com/mycompany/mycompanyTax/controller/TaxController.java
+++ src/main/java/com/mycompany/mycompanyTax/controller/TaxController.java
@@ -4,10 +4,12 @@ import com.mycompany.mycompanyTax.model.TaxReq;
 import com.mycompany.mycompanyTax.model.TaxResp;
 import com.mycompany.mycompanyTax.service.TaxService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1")
 public class TaxController {
